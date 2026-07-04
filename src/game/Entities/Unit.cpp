6da48d1efa0cf27b77d19e5c163c7a8139a6722c
PlayDargoon <VongolaIvanrogue@gmail.com>
2026-07-04 14:39:35 +1100

    feat: integrate cmangos-modules system with PlayerBots, Hardcore support
    - Apply classic.patch from cmangos-modules for module hooks
    - Fix MailHandler OnSendMail call (4 args: draft, player, receiver, cost)
    - Update PlayerBots URL to Sakhwoow/ClassicPlayerBots fork
    - Override HARDCORE module URL to Sakhwoow/cmangos-hardcore fork

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -56,6 +56,10 @@
  #include "Metric/Metric.h"
 #endif
 
+#ifdef ENABLE_MODULES
+#include "ModuleMgr.h"
+#endif
+
 #include <math.h>
 #include <limits>
 #include <array>
@@ -1007,6 +1011,10 @@ uint32 Unit::DealDamage(Unit* dealer, Unit* victim, uint32 damage, CleanDamage c
 
     DEBUG_FILTER_LOG(LOG_FILTER_DAMAGE, "DealDamageEnd returned %d damage", damage);
 
+#ifdef ENABLE_MODULES
+    sModuleMgr.OnDealDamage(dealer, victim, health, damage);
+#endif
+
     return damage;
 }
 
@@ -1160,6 +1168,10 @@ void Unit::Kill(Unit* killer, Unit* victim, DamageEffectType damagetype, SpellEn
         {
             DEBUG_FILTER_LOG(LOG_FILTER_DAMAGE, "SET JUST_DIED");
             victim->SetDeathState(JUST_DIED);
+
+#ifdef ENABLE_MODULES
+            sModuleMgr.OnDeath(playerVictim, killer);
+#endif
         }
 
         // playerVictim was in duel, duel must be interrupted
@@ -1192,6 +1204,10 @@ void Unit::Kill(Unit* killer, Unit* victim, DamageEffectType damagetype, SpellEn
     // stop combat
     DEBUG_FILTER_LOG(LOG_FILTER_DAMAGE, "DealDamageAttackStop");
     victim->CombatStop();
+
+#ifdef ENABLE_MODULES
+    sModuleMgr.OnKill(killer, victim);
+#endif
 }
 
 void Unit::HandleDamageDealt(Unit* dealer, Unit* victim, uint32& damage, CleanDamage const* cleanDamage, DamageEffectType damagetype, SpellSchoolMask damageSchoolMask, SpellEntry const* spellInfo, bool duel_hasEnded)
@@ -3216,6 +3232,11 @@ float Unit::CalculateEffectiveDodgeChance(const Unit* attacker, WeaponAttackType
 {
     float chance = 0.0f;
 
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateEffectiveDodgeChance(this, attacker, attType, ability, chance))
+        return chance;
+#endif
+
     chance += GetDodgeChance();
     // Own chance appears to be zero / below zero / unmeaningful for some reason (debuffs?): skip calculation, unit is incapable
     if (chance < 0.005f)
@@ -3239,6 +3260,11 @@ float Unit::CalculateEffectiveDodgeChance(const Unit* attacker, WeaponAttackType
 float Unit::CalculateEffectiveParryChance(const Unit* attacker, WeaponAttackType attType, const SpellEntry* ability) const
 {
     float chance = 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateEffectiveParryChance(this, attacker, attType, ability, chance))
+        return chance;
+#endif
 
     if (attType == RANGED_ATTACK)
         return 0.0f;
@@ -3271,6 +3297,11 @@ float Unit::CalculateEffectiveParryChance(const Unit* attacker, WeaponAttackType
 float Unit::CalculateEffectiveBlockChance(const Unit* attacker, WeaponAttackType attType, const SpellEntry* ability) const
 {
     float chance = 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateEffectiveBlockChance(this, attacker, attType, ability, chance))
+        return chance;
+#endif
 
     chance += GetBlockChance();
     // Own chance appears to be zero / below zero / unmeaningful for some reason (debuffs?): skip calculation, unit is incapable
@@ -3719,6 +3750,12 @@ float Unit::GetMissChance(const SpellEntry* entry, SpellSchoolMask schoolMask) c
 float Unit::CalculateEffectiveCritChance(const Unit* victim, WeaponAttackType attType, const SpellEntry* ability) const
 {
     float chance = 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateEffectiveCritChance(this, victim, attType, ability, chance))
+        return chance;
+#endif
+
     chance += (ability ? GetCritChance(ability, SPELL_SCHOOL_MASK_NORMAL) : GetCritChance(attType));
     // Own chance appears to be zero / below zero / unmeaningful for some reason (debuffs?): skip calculation, unit is incapable
     if (chance < 0.005f)
@@ -3756,6 +3793,11 @@ float Unit::CalculateEffectiveCritChance(const Unit* victim, WeaponAttackType at
 float Unit::CalculateEffectiveMissChance(const Unit *victim, WeaponAttackType attType, const SpellEntry* ability) const
 {
     float chance = 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateEffectiveMissChance(this, victim, attType, ability, m_currentSpells, SPELL_PARTIAL_RESIST_DISTRIBUTION, chance))
+        return chance;
+#endif
 
     chance += (ability ? victim->GetMissChance(ability, SPELL_SCHOOL_MASK_NORMAL) : victim->GetMissChance(attType));
     // Victim's own chance appears to be zero / below zero / unmeaningful for some reason (debuffs?): skip calculation, unit can't be missed
@@ -3844,6 +3886,12 @@ float Unit::CalculateSpellMissChance(const Unit* victim, SpellSchoolMask schoolM
         return 0.0f;
 
     float chance = 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnCalculateSpellMissChance(this, victim, schoolMask, spell, chance))
+        return chance;
+#endif
+
     const float minimum = 1.0f; // Pre-WotLK: unavoidable spellInfo miss is at least 1%
 
     if (spell->HasAttribute(SPELL_ATTR_EX3_NORMAL_RANGED_ATTACK) || spell->DmgClass == SPELL_DAMAGE_CLASS_MELEE || spell->DmgClass == SPELL_DAMAGE_CLASS_RANGED)
@@ -6887,6 +6935,10 @@ int32 Unit::DealHeal(Unit* pVictim, uint32 addhealth, SpellEntry const* spellInf
     // Script Event HealedBy
     if (pVictim->AI())
         pVictim->AI()->HealedBy(this, addhealth);
+
+#ifdef ENABLE_MODULES
+    sModuleMgr.OnDealHeal(unit, pVictim, gain, addhealth);
+#endif
 
     return gain;
 }
@@ -11749,6 +11801,11 @@ float Unit::GetAttackDistance(Unit const* target) const
     float aggroRate = sWorld.getConfig(CONFIG_FLOAT_RATE_CREATURE_AGGRO);
     if (aggroRate == 0)
         return 0.0f;
+
+#ifdef ENABLE_MODULES
+    if (sModuleMgr.OnGetAttackDistance(this, target, aggroRate))
+        return aggroRate;
+#endif
 
     uint32 playerlevel = target->GetLevelForTarget(this);
     uint32 creaturelevel = GetLevelForTarget(target);
